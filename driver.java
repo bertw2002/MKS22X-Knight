03ebaa85287cdd93ee3b8aadf3e5c89900c4f255
bertw2002 <awan00@stuy.edu>
2019-02-15 11:46:58 -0500

    classwork

--- driver.java
+++ driver.java
@@ -2,5 +2,6 @@ public class driver{
   public static void main(String[] args){
     KnightBoard board = new KnightBoard(10, 10);
     System.out.println(board);
+    System.out.println(board.solve(0, 0));
   }
 }
